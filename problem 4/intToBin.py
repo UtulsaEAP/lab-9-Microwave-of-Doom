6f2c49258d8778657b90a9df202fb3d88a731b04
Microwave-of-Doom <jacksonmoseman04@gmail.com>
2024-11-02 11:42:03 -0500

    prob 5 done prob 4 is being weird

--- problem 4/intToBin.py
+++ problem 4/intToBin.py
@@ -3,16 +3,14 @@ def int_to_reverse_binary(num1):
 #write your while loop here
     #while num1 > 0:
         #write your code
-
-    return binary_val;
+    while num1 > 0:
+        binary_val += str(num1 % 2)
+        num1 = num1 // 2
+    return binary_val
 
 
 def string_reverse(input_string): 
-    reverse_input = ''
-    
-   #write your for loop here
-    
-    return reverse_input
+    return input_string[::-1]
 
 if __name__ == '__main__':
     user_input = int(input())
--- problem 5/varSwap.py
+++ problem 5/varSwap.py
@@ -1,7 +1,7 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
-   
-   return user_val1, user_val2, user_val3, user_val4
+   swapped_values = (user_val2,user_val1,user_val4,user_val3)
+   return swapped_values
 
 if __name__ == '__main__':   
    user_input1 = int(input())
@@ -10,5 +10,7 @@ if __name__ == '__main__':
    user_input4 = int(input())
    #store output for every input here
    #print those output
+   swapped_values = swap_values(user_input1, user_input2, user_input3, user_input4)
 
+   print(f"{swapped_values[0]} {swapped_values[1]} {swapped_values[2]} {swapped_values[3]}")
  
